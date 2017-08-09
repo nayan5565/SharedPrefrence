ebacd8f2650239e11d90ac519f93991cc9746b18
nayan5565 <nayan5565@gmail.com>
2017-08-10 00:29:53 +0600

    init

---
+++ app/src/main/java/com/nayan/sheredprefrence/MainActivity.java
@@ -0,0 +1,62 @@
+package com.nayan.sheredprefrence;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedpreferences;
+    TextView name;
+    TextView email;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = (TextView) findViewById(R.id.etName);
+        email = (TextView) findViewById(R.id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+
+    }
+
+    public void StoreData(View view) {
+        String n = name.getText().toString();
+        String e = email.getText().toString();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, n);
+        editor.putString(Email, e);
+        editor.commit();
+    }
+
+    public void clear(View view) {
+        name.setText("");
+        email.setText("");
+
+    }
+
+    public void GetData(View view) {
+//        sharedpreferences = getSharedPreferences(mypreference,Context.MODE_PRIVATE);
+
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+    }
+}
